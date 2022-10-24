2596500557cfe6eca028c548bb80609dd57459c2
DIpen Kalal <171080107009.acet>
2022-10-24 18:13:33 -0400

    test commit

---
+++ lib/splash_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    body: Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomRight,
+          colors: [
+            Color(0xffffff),
+            Color(0xFFFF00)]
+        )
+      ),
+    ),
+
+    );
+  }
+}
