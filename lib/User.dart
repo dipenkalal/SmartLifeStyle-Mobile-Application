203a5a9df4a6b758e98b5c3c7dc39e01348a7495
DIpen Kalal <171080107009.acet>
2022-10-26 19:42:16 -0400

    backup

---
+++ lib/User.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+Class User{
+  final String imagePath;
+  final String name;
+  final String email;
+  final int number;
+  final double date;
+  final double time;
+  final bool isDarkMode;
+
+const User({
+
+    required this.imagePath,
+    required this.name,
+required this.email,
+required this.number,
+required this.date,
+required this.time,
+required this.isDarkMode,
+
+});
+
+}
+
